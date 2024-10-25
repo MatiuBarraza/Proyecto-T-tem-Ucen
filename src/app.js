5d25897d7324a564ac9ded6c8bfd541f585729fa
krezx <jotazocarvajal@gmail.com>
2024-10-24 23:51:35 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login/Login';
-import MainApp from './pages/app/App';
-
+import React, { useState } from 'react';
+import Header from './components/Header';
+import ChatWindow from './components/ChatWindow';
+import MessageInput from './components/MessageInput';
+import './App.css';
+import axios from 'axios';
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<MainApp />} />
-      </Routes>
-    </Router>
-  );
+    const [messages, setMessages] = useState([]);
+    const suggestions = [
+        "¿Cómo hago mi cambio de claves?",
+        "¿Qué debo hacer para congelar?",
+        "En caso de tener licencia, ¿qué debo hacer?",
+        "¿Cómo puedo imprimir dentro de la universidad?",
+    ];
+
+    const sendMessage = async (msg) => {
+        // Añadir el mensaje del usuario al chat
+        setMessages((prevMessages) => [...prevMessages, { text: msg, sender: "user" }]);
+
+        try {
+            const response = await axios.post('http://localhost:5000/preguntar', { pregunta: msg });
+            const botResponse = response.data.respuesta;
+            // Añadir la respuesta del bot al chat
+            setMessages((prevMessages) => [...prevMessages, { text: botResponse, sender: "bot" }]);
+        } catch (error) {
+            console.error("Error enviando la pregunta:", error);
+            setMessages((prevMessages) => [...prevMessages, { text: "Error al obtener respuesta.", sender: "bot" }]);
+        }
+    };
+
+    const selectSuggestion = (suggestion) => {
+        sendMessage(suggestion);
+    };
+
+    return (
+        <div className="app">
+            <Header />
+            <ChatWindow messages={messages} suggestions={suggestions} selectSuggestion={selectSuggestion} />
+            <MessageInput sendMessage={sendMessage} />
+        </div>
+    );
 };
 
 export default App;
